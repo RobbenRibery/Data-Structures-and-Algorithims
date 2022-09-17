7ca5cfb66cd6b289e09a2c8b41c5cc18b4dd73da
RobbenRibery <runodng.liu@outlook.com>
2022-09-17 17:16:54 +0100

    ksjdhkj

--- Arrays-and-Hashing/product_of_array_except_itself.py
+++ Arrays-and-Hashing/product_of_array_except_itself.py
@@ -1,4 +1,5 @@
-def productExceptSelf(nums): 
+from typing import List
+def productExceptSelf(nums:List[int]) -> List[int]: 
 
     """
     :type nums: List[int]
@@ -6,12 +7,24 @@ def productExceptSelf(nums):
     :solution must be provided without using divsion method 
 
     Solution: 
-    - Go through the entire array from 
+    - Initialise the array as all elements equal to 1, product default to 1, subject to modifications 
+    - Go through the entire array from left hand side to right hand side 
+       - (Note here we omit the first element when we start from left, as nothing is on its left) 
+       - left hand side product = left hand side product from previous position * the value of the previous position 
+
+
+    - Initilasie a number to keep track of the accumulative product on the RHS 
+    - Go thoguh the entire array from right hand side to left hand sde 
+       - (Note here we omit the first element when we start from right, as nothing is on its right) 
+       -  update the result array, such that 
+        - final result = left hand side product (re[i]) * accumulative product from right 
+        - increase the accumulative product value as we iterate through 
 
     Complexity: 
     - N = len(nums)
-    - TC: 0(N)
-    - SC: 0(N) / 0(1) if discount the output aray 
+    - TC: 0(N-1) ~ O(N)
+    - SC: 0(N) 
+          OR 0(1) if discount the storage for the output array 
     """ 
     res = [1]*len(nums)
 
@@ -33,42 +46,9 @@ if __name__ == '__main__':
 
     nums_1 = [1,2,3,4]
     nums_2 = [-1,1,0,-3,3]
-    # - if 
-    ## one position is zero, the rest of positions are all zero 
-
-    # - else
-    ## 1 - 2 * 3 * 4 * 0
-    ## 2 - 1 * 3 * 4 * 0
-    ## 4 - 1 * 2 * 3 * 0
-    ## 3 - 1 * 2 * 4 * 0
-    ## 0 - 1 * 2 * 3 * 4 
+    
     print(productExceptSelf(nums_1))
     print(productExceptSelf(nums_2))
-
-    ## left to write pass
-
-    # i: 0-> len(nums)
-
-    ## i = 0 -> lhs[i] *= 1 
-    ## i = 1 -> lhs[i] = lhs[i-1] * nums[i-1]
-
-
-    ### lhs = [1,1,2,6]
-
-    # 3 -> 2 * 4 
-    # 2 -> 1 res[i] * 3 nums[i-1] * 4 nums[i-1]
-     
-
-
-    ## right to left pass 
-
-    # i: len(nums)-1 -> 0 
-    ## i = len(nums) -1 -> lhs[i] *= 1 
-    ## i = len(nums) -2 -> lhs[i] = lhs[i+1] * nums[i+1]
-
-
-    # return lhs 
-
 
 
 
