b0afd5dafb66cb9319265dbd7a3a3c3244954f62
RobbenRibery <runodng.liu@outlook.com>
2022-09-20 20:18:51 +0100

    skdf

---
+++ two-pointers/three_sum_unsorted.py
@@ -0,0 +1,16 @@
+class Solution(object): 
+
+    def ThreeSum(self, nums): 
+
+        res = []
+        
+        return res
+
+
+if __name__ == "__main__": 
+
+    nums = []
+
+    sol = Solution()
+
+    print(sol.ThreeSum(nums))
