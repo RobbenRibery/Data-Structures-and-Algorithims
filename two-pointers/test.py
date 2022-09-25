2a59e21e523c0137552228ccdeafcf7899424e2c
RobbenRibery <runodng.liu@outlook.com>
2022-09-25 20:10:58 +0100

    three sum N2

--- two-pointers/test.py
+++ two-pointers/test.py
@@ -1,1 +1,7 @@
-print('1'.isalnum())
+nums = [-1,0,1,2,-1,-4]
+left_index = 0
+right_index = len(nums)-1
+
+temp_nums = nums[:left_index] + nums[left_index+1:right_index] + nums[right_index+1:]
+
+print(temp_nums)
